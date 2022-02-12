612b371dfca7887de01b215204e514786c45be2b
Subzero2004 <germannaz_@hotmail.com>
2022-02-12 13:14:05 -0300

    A

---
+++ src/main/java/net/subzero/ageofmechanics/item/ModItemGroup.java
@@ -0,0 +1,12 @@
+package net.subzero.ageofmechanics.item;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.subzero.ageofmechanics.AgeOfMechanics;
+
+public class ModItemGroup {
+    public static final ItemGroup STEEL = FabricItemGroupBuilder.build(new Identifier(AgeOfMechanics.MOD_ID, "steel"),
+            () -> new ItemStack(ModItems.STEEL_INGOT));
+}
